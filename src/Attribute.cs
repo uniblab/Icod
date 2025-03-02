d12c66764373ed3ac62c31e9db509b74f58f53cb
uniblab <uniblab@hotmail.com>
2025-03-01 21:42:02 -0500

    Add xmldoc

--- src/Attribute.cs
+++ src/Attribute.cs
@@ -7,7 +7,7 @@
 This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.  
+ version 3 of the License, or (at your option) any later version.  
 
 This library is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,14 +19,14 @@ You should have received a copy of the GNU Lesser General Public
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-namespace Icod { 
+namespace Icod {
 
 	/// <include file='..\doc\Icod.xml' path='types/type[@name="Icod.Attribute"]/member[@name=""]/*'/>
 	[System.Serializable]
-	public abstract class Attribute : System.Attribute, System.Runtime.Serialization.ISerializable { 
+	public abstract class Attribute : System.Attribute, System.Runtime.Serialization.ISerializable {
 
 		/// <include file='..\doc\Icod.xml' path='types/type[@name="Icod.Attribute"]/member[@name="#ctor"]/*'/>
-		protected Attribute() : base() { 
+		protected Attribute() : base() {
 		}
 
 		/// <include file='..\doc\Icod.xml' path='types/type[@name="Icod.Attribute"]/member[@name="#ctor(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)"]/*'/>
